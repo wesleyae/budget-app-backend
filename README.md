2107cbd434e1c3663754b8726b0df876a2ce0b21
Wesley Eledui <wefilmer@gmail.com>
2018-09-01 15:53:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Budget App REST API
+This is the REST API for my budget app.
+
+Prereqs:
+- [Python 3.7+](https://www.python.org/)
+- [Pipenv](https://pipenv.readthedocs.io/en/latest/)
+
+Development Setup:
+- run `pipenv install`
+- run `pipenv run python api.py`
---
+++ api.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins='*')
+api = Api(app)
+
+ENVELOPES = []
+
+class Envelope(Resource):
+    def get(self, envelope_name):
+        return {envelope_name: ENVELOPES[envelope_name]}
+
+    def put(self, envelope_name):
+        ENVELOPES.append({
+            'name': envelope_name,
+            'amount': request.get_json()['amount']
+        })
+        return ENVELOPES[-1]
+
+class EnvelopeList(Resource):
+    def get(self):
+        return ENVELOPES
+
+api.add_resource(EnvelopeList, '/envelopes')
+api.add_resource(Envelope, '/<string:envelope_name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
